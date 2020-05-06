fcfc1fa8724805e4b664468d8796b839f98ba81c
aileenliu63 <40475451+aileenliu63@users.noreply.github.com>
2020-05-06 15:45:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/diistance/GeofenceSpecifics.java
@@ -0,0 +1,24 @@
+package com.example.diistance;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class GeofenceSpecifics extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.adjustments);
+    }
+
+
+
+}
